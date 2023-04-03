fcadb5e71473e07386132a10a2dd59b98a8281ae
xMatoi <54566965+xMatoi@users.noreply.github.com>
2023-04-02 23:15:50 -0600

    Add files via upload

---
+++ Motoristas/proyecto2cliente/js/masteer.js
@@ -0,0 +1,44 @@
+const cambio = () => { 
+    document.getElementById('jjj').style.display="none";
+    document.getElementById('jj').style.display="block";
+
+ }
+
+ const cambio2 = () => {
+
+    document.getElementById('jj').style.display="none";
+    document.getElementById('jjj').style.display="block";
+ }
+ const cambio3 = () => {
+
+    document.getElementById('jj').style.display="none";
+    document.getElementById('jjj').style.display="none";
+    document.getElementById('jjjj').style.display="block";
+
+ }
+ 
+ const cambio4 = () => {
+    document.getElementById('llll').style.display="block";
+    document.getElementById('mmmm').style.display="none";
+    document.getElementById('zzzz').style.display="none";
+
+ }
+
+ const cambio5 = () => {
+    document.getElementById('llll').style.display="none";
+    document.getElementById('zzzz').style.display="block";
+    document.getElementById('mmmm').style.display="none";
+
+ }
+
+ const cambio6 = () => {
+    document.getElementById('llll').style.display="none";
+    document.getElementById('zzzz').style.display="none";
+    document.getElementById('mmmm').style.display="block";}
+
+ const salir = () => {
+    document.getElementById('jjjj').style.display="none";
+    document.getElementById('jjj').style.display="block";
+     }
+
+
